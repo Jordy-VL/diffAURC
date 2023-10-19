11e7ab2fa1a69cc5a2e0573d509d8f5b3a922f49
Jordy Van Landeghem <jordy.vlan@gmail.com>
2023-10-19 17:09:04 +0200

    separated loss to be used in other projects

---
+++ src/AURC_loss.py
@@ -0,0 +1,77 @@
+from collections import OrderedDict
+import numpy as np
+from scipy.special import softmax
+import torch
+
+##### g: confidence scoring functions #####
+
+
+def entropy(x):
+    exp_x = np.exp(x)
+    A = np.sum(exp_x, axis=-1)  # sum of exp(x_i)
+    B = np.sum(x * exp_x, axis=-1)  # sum of x_i * exp(x_i)
+    return np.log(A) - B / A
+
+
+def entropy_torch(x):
+    exp_x = torch.exp(x)
+    A = torch.sum(exp_x, dim=-1)  # sum of exp(x_i)
+    B = torch.sum(x * exp_x, dim=-1)  # sum of x_i * exp(x_i)
+    return torch.log(A) - B / A
+
+
+def top12_margin(x):
+    """top-1 - top-2"""
+    values, _ = torch.topk(x, k=2, dim=-1)
+    if x.ndim == 1:
+        return values[0] - values[1]
+    return values[:, 0] - values[:, 1]
+
+
+def top12_margin_np(x):
+    values = np.sort(x, axis=-1)
+    if x.ndim == 1:
+        return values[0] - values[1]
+    return values[:, 0] - values[:, 1]
+
+
+CSF_dict = OrderedDict(
+    {
+        "msp": lambda x: torch.max(torch.nn.functional.softmax(x, dim=-1), -1)[0],  # maximum softmax probability
+        "entropy": lambda x: -entropy_torch(x),  # negative entropy
+        "margin": lambda x: top12_margin(x),  # top-1 - top-2
+        "msp_np": lambda x: np.max(softmax(x, axis=-1), -1),  # maximum softmax probability
+        "entropy_np": lambda x: -entropy(x),  # negative entropy
+        "margin_np": lambda x: top12_margin_np(x),  # top-1 - top-2
+    }
+)
+
+
+##### differentiable loss #####
+
+
+class AURCLoss(torch.nn.Module):
+
+    """PyTorch version of differentiable AURC (https://arxiv.org/pdf/1805.08206.pdf)
+
+    \operatorname{AURC}(f) := \frac{1}{n} \sum_{i=1}^{n-1} \frac{\sum_{j=i+1}^n\ell([f(x_{i:n})]_{1:k},y_j)}{(n-i)}
+
+    This version has time complexity O(N), yet does not take into account duplicate values in g(f_X)
+    """
+
+    def __init__(self, g=CSF_dict["msp"], loss_function=torch.nn.CrossEntropyLoss()):
+        super(AURCLoss, self).__init__()
+        self.g = g
+        self.loss_function = loss_function
+
+    def forward(self, input, target):
+        B = len(target)
+        indices_sorted = torch.argsort(self.g(input), descending=True)
+        sorted_f_X = input[indices_sorted]
+        sorted_y = target[indices_sorted]
+        final_sum = 0
+        partial_loss = self.loss_function(sorted_f_X[0], sorted_y[0])
+        for i in range(1, B):
+            final_sum += partial_loss / i
+            partial_loss += self.loss_function(sorted_f_X[i], sorted_y[i])
+        return final_sum / B
--- src/test_differentiability_AURC.py
+++ src/test_differentiability_AURC.py
@@ -5,10 +5,10 @@ import pickle
 from sklearn.model_selection import train_test_split
 import torch
 
+from AURC_loss import AURCLoss, CSF_dict
 from AURC_implementations import IMPLEMENTATIONS, runtime_wrapper
 
 ##### data generation for testing #####
-
 
 def generate_random_data():
     num_samples = 10
@@ -72,79 +72,6 @@ def load_CIFAR_logits(path="../data/resnet110_c10_logits.p"):
     return (p_val, y_val), (p_test, y_test)
 
 
-##### g: confidence scoring functions #####
-
-
-def entropy(x):
-    exp_x = np.exp(x)
-    A = np.sum(exp_x, axis=-1)  # sum of exp(x_i)
-    B = np.sum(x * exp_x, axis=-1)  # sum of x_i * exp(x_i)
-    return np.log(A) - B / A
-
-
-def entropy_torch(x):
-    exp_x = torch.exp(x)
-    A = torch.sum(exp_x, dim=-1)  # sum of exp(x_i)
-    B = torch.sum(x * exp_x, dim=-1)  # sum of x_i * exp(x_i)
-    return torch.log(A) - B / A
-
-
-def top12_margin(x):
-    """top-1 - top-2"""
-    values, _ = torch.topk(x, k=2, dim=-1)
-    if x.ndim == 1:
-        return values[0] - values[1]
-    return values[:, 0] - values[:, 1]
-
-
-def top12_margin_np(x):
-    values = np.sort(x, axis=-1)
-    if x.ndim == 1:
-        return values[0] - values[1]
-    return values[:, 0] - values[:, 1]
-
-
-CSF_dict = OrderedDict(
-    {
-        "msp": lambda x: torch.max(torch.nn.functional.softmax(x, dim=-1), -1)[0],  # maximum softmax probability
-        "entropy": lambda x: -entropy_torch(x),  # negative entropy
-        "margin": lambda x: top12_margin(x),  # top-1 - top-2
-        "msp_np": lambda x: np.max(softmax(x, axis=-1), -1),  # maximum softmax probability
-        "entropy_np": lambda x: -entropy(x),  # negative entropy
-        "margin_np": lambda x: top12_margin_np(x),  # top-1 - top-2
-    }
-)
-
-
-##### differentiable loss #####
-
-
-class AURCLoss(torch.nn.Module):
-
-    """PyTorch version of differentiable AURC (https://arxiv.org/pdf/1805.08206.pdf)
-
-    \operatorname{AURC}(f) := \frac{1}{n} \sum_{i=1}^{n-1} \frac{\sum_{j=i+1}^n\ell([f(x_{i:n})]_{1:k},y_j)}{(n-i)}
-
-    This version has time complexity O(N), yet does not take into account duplicate values in g(f_X)
-    """
-
-    def __init__(self, g=CSF_dict["msp"], loss_function=torch.nn.CrossEntropyLoss()):
-        super(AURCLoss, self).__init__()
-        self.g = g
-        self.loss_function = loss_function
-
-    def forward(self, input, target):
-        B = len(target)
-        indices_sorted = torch.argsort(self.g(input), descending=True)
-        sorted_f_X = input[indices_sorted]
-        sorted_y = target[indices_sorted]
-        final_sum = 0
-        partial_loss = self.loss_function(sorted_f_X[0], sorted_y[0])
-        for i in range(1, B):
-            final_sum += partial_loss / i
-            partial_loss += self.loss_function(sorted_f_X[i], sorted_y[i])
-        return final_sum / B
-
 
 if __name__ == "__main__":
     # add argparse for N and K
@@ -185,6 +112,8 @@ if __name__ == "__main__":
 
         # test metrics vs. loss function
         for _, impl in IMPLEMENTATIONS:
+            if args.N > 1000 and impl.__name__ == "naive":
+                continue
             g = CSF_dict[CSF + "_np"]
             v, t = runtime_wrapper(impl, p_test.detach().numpy(), g, y_test.numpy())
             print(f"{CSF}: loss: {loss.item()} vs. {impl.__name__}: {v} taking {round(t,4)}s")
